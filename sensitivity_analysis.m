898b9428ea6abf7ee2cbf8df9c3cae11496edc66
Samir Unni <srunni@gmail.com>
2013-11-19 20:12:00 -0500

    Add time to finish prediction to sensitivity analysis

--- sensitivity_analysis.m
+++ sensitivity_analysis.m
@@ -7,13 +7,22 @@ function freqs = sensitivity_analysis(oscillator, param1, param1_range, param2, 
 
     freqs = zeros(length(param1_range), length(param2_range));
 
+    tic;
+    total_sims = length(param1_range)*length(param2_range);
+
     for a = 1:length(param1_range)
         for b = 1:length(param2_range)
             param_vals = struct(param1, param1_range(a), param2, param2_range(b));
             eval(sprintf('freqs(a,b) = simulate_%s(param_vals);', oscillator));
 
+            time_elapsed = toc;
+            sims_finished = (a-1)*length(param2_range)+b;
+            sims_remaining = total_sims - sims_finished;
+            time_remaining = (time_elapsed/sims_finished)*sims_remaining;
+
             % Display progress
-            fprintf('Completed %i/%i simulations...\n', (a-1)*length(param2_range)+b , length(param1_range)*length(param2_range));
+            fprintf('Completed %i/%i simulations (%is elapsed, ~%is remaining)\n',...
+                sims_finished, total_sims, round(time_elapsed), round(time_remaining));
         end
     end
 end
