1f0ca885075126f8641ce74e2e74642ad49941d9
Robert-Collopy <rjc25@duke.edu>
2013-11-21 18:07:06 -0500

    Added Time and y0 to try to copy repressilator
    Still not working - input argument errors.

--- ODE_repressilator_pfl.m
+++ ODE_repressilator_pfl.m
@@ -1,9 +1,9 @@
-function dydt = ODE_repressilator_pfl(alpha, beta, alpha0, n, k)
+function dydt = ODE_repressilator_pfl(Time, y0, alpha, beta, alpha0, n, k)
 
-    dydt = [alpha0 + alpha./(1.0 + y(6).^n) - y(1) + (k.*(alpha0 - y(1)).*alpha)./(1.0 + y(2).^n);
-            beta.*(y(1) - y(2));
-            alpha0 + alpha./(1.0 + y(2).^n) - y(3);
-            beta.*(y(3) - 0.19.*y(4));
-            alpha0 + alpha./(1.0 + y(4).^n) - y(5);
-            beta.*(y(5) - y(6));
+    dydt = [alpha0 + alpha./(1.0 + y0(6).^n) - y0(1) + (k.*(alpha0 - y0(1)).*alpha)./(1.0 + y0(2).^n);
+            beta.*(y0(1) - y0(2));
+            alpha0 + alpha./(1.0 + y0(2).^n) - y0(3);
+            beta.*(y0(3) - 0.19.*y0(4));
+            alpha0 + alpha./(1.0 + y0(4).^n) - y0(5);
+            beta.*(y0(5) - y0(6));
             ];
