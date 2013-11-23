4f31ed46f85b00e2a6576a80b46a82e900a93b49
Samir Unni <srunni@gmail.com>
2013-11-23 16:22:11 -0500

    Further fix contour plotting

--- heatmap.m
+++ heatmap.m
@@ -5,7 +5,7 @@ function freqs = heatmap(oscillator, param1, param1_range, param2, param2_range)
     figure(1);
     contourf(param2_range, param1_range, freqs);
     colorbar;
-    xlabel(param1);
-    ylabel(param2);
+    ylabel(param1);
+    xlabel(param2);
     title(sprintf('Frequency Heatmap (%s)', oscillator));
 end
