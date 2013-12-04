9ddc1ed92d8614624792ef2ab322a75ef7c69300
Samir Unni <srunni@gmail.com>
2013-12-03 23:02:16 -0500

    Change parallel simulation code for use with new blocker approach

--- launch_parallel_sims.sh
+++ launch_parallel_sims.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 username="su20";
-total_sims=100;
+total_sims=25;
 
 for i in `seq 1 $total_sims`; do
-  matlab_cmd="matlab -nodesktop -nosplash -nojvm -r \\\"run_parallel_sim($i, $total_sims)\\\""
-  server_num=$(($i/4+1));
-  server_num=$(printf "%02i" $server_num);
+    matlab_cmd="matlab -nodesktop -nosplash -nojvm -r \\\"run_parallel_sim($i, $total_sims)\\\""
+    server_num=$(($i/1+1));
+    server_num=$(printf "%02i" $server_num);
 
-  cmd=$(printf "ssh -t %s@teer%s.oit.duke.edu \"cd bme_574-engineering_robust_oscillators;\n" $username $server_num);
-  close="\" &";
-  eval $cmd$matlab_cmd$close
+    cmd=$(printf "ssh -t %s@teer%s.oit.duke.edu \"cd bme_574-engineering_robust_oscillators;\n" $username $server_num);
+    close="\" &";
+    eval $cmd$matlab_cmd$close
 done
--- run_parallel_sim.m
+++ run_parallel_sim.m
@@ -1,12 +1,19 @@
 function run_parallel_sim(num, total_sims)
-    oscillator = 'smolon';
-    param1 = 'sigma';
-    param1_range = linspace(0, 3*1.5, total_sims);
-    param2 = 'gamma_y';
-    param2_range = linspace(0, 3*0.036, total_sims);
+    dimension = sqrt(total_sims);
+
+    oscillator = 'metabolator_mod';
+
+    param1 = 'a3';
+    param1_range = linspace(0, 2*3, 5);
+    param1_val = param1_range(floor((num-1)/dimension)+1);
+
+    param2 = 'B';
+    param2_range = linspace(0, 20, 5);
+    param2_val = param2_range(mod(num-1, dimension)+1);
+
+    freq = eval(sprintf('simulate_%s(struct(param1, param1_val, param2, param2_val))', oscillator));
+
+    fprintf('Completed simulation %i (%s = %g, %s = %g)', num, param1, param1_val, param2, param2_val)
     
-    param1_val = param1_range(num);
-    freqs = sensitivity_analysis(oscillator, param1, [param1_val], param2, param2_range);
-    
-    save(sprintf('%03i.mat', num), 'freqs', 'oscillator', 'param1', 'param1_range', 'param2', 'param2_range', 'param1_val');
+    save(sprintf('%03i.mat', num), 'freq', 'oscillator', 'param1', 'param1_val', 'param2', 'param2_val');
 end
