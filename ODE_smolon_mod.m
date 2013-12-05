ca571f0d5a9e255c199b4b89f05bcc9e60378911
Samir Unni <srunni@gmail.com>
2013-12-05 14:58:18 -0500

    Fix modified Smolon model

--- ODE_smolon_mod.m
+++ ODE_smolon_mod.m
@@ -2,6 +2,6 @@ function dydt = ODE_smolon_mod(Time, y, sigma, tau_y, gamma_x, gamma_y, alpha, d
 
     delta = 1 - (delta_max - 1)./(1 + exp(D_half)) + (delta_max - 1)./(1 + exp(-(D-D_half)));
 
-    dydt = [(1 + y(1).^2 + alpha.*sigma.*y(1)^4)./((1 + y(1).^2 + sigma.*y(1).^4).*(1 + y(2).^4)) - (gamma_x + delta).*y(1);
-            ((1 + y(1).^2 + alpha.*sigma.*y(1)^4)./((1 + y(1).^2 + sigma.*y(1).^4).*(1 + y(2).^4)) - gamma_y.*y(2))./tau_y;
+    dydt = [(1 + y(1).^2 + delta*alpha.*sigma.*y(1)^4)./((1 + y(1).^2 + sigma.*y(1).^4).*(1 + y(2).^4)) - gamma_x.*y(1);
+            ((1 + y(1).^2 + delta*alpha.*sigma.*y(1)^4)./((1 + y(1).^2 + sigma.*y(1).^4).*(1 + y(2).^4)) - gamma_y.*y(2))./tau_y;
            ];
