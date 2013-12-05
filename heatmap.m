5c54aaa795659b3402fb4ff2d23441aaf26a5077
Samir Unni <srunni@gmail.com>
2013-12-05 12:51:40 -0500

    Fix heatmap plotting

--- heatmap.m
+++ heatmap.m
@@ -1,11 +1,11 @@
 function freqs = heatmap(oscillator, param1, param1_range, param2, param2_range)
     freqs = sensitivity_analysis(oscillator, param1, param1_range, param2, param2_range);
 
-    clf;
+    clf; close all;
     figure(1);
     contourf(param2_range, param1_range, freqs);
     colorbar;
     ylabel(param1);
     xlabel(param2);
-    title(sprintf('Frequency Heatmap (%s)', oscillator));
+    title(sprintf('Frequency Heatmap (%s)', strrep(oscillator, '_', '\_')));
 end
